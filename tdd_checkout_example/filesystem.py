46692c29b9cdf0d3cb06780650264585ce79ab7a
Michel Nossin <michel.nossin@gmail.com>
2018-12-08 15:13:30 +0100

    Test for exception by using mock

--- tdd_checkout_example/filesystem.py
+++ tdd_checkout_example/filesystem.py
@@ -1,8 +1,13 @@
+import os
+
+
 class FilesystemHandler:
     def __init__(self):
         pass
 
     def read_from_file(self, filename):
+        if not os.path.exists("filename"):
+            raise Exception("Bad filename")
         infile = open(filename, "r")
         line = infile.readline()
         return line
--- tdd_checkout_example/tests/test_filesystem.py
+++ tdd_checkout_example/tests/test_filesystem.py
@@ -9,9 +9,23 @@ def fs_handler():
     return fs.FilesystemHandler()
 
 
-def test_read_from_file_returns_correct_string(fs_handler, monkeypatch):
+@pytest.fixture()
+def mock_read_from_file(fs_handler, monkeypatch):
     def mock_read_from_file(filename):
+        if filename == "badfile":
+            raise Exception("Bad filename")
         return "CORRECT_STRING"
     monkeypatch.setattr(fs_handler, 'read_from_file', mock_read_from_file)
+    return mock_read_from_file
+
+
+def test_read_from_file_returns_correct_string(
+        fs_handler, mock_read_from_file, monkeypatch):
     result = fs_handler.read_from_file("somefile")
     assert result == "CORRECT_STRING"
+
+
+def test_read_from_file_throws_exception_badfile(
+        fs_handler, mock_read_from_file, monkeypatch):
+    with raises(Exception):
+        fs_handler.read_from_file("badfile")
